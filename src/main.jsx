24d60569bc38853f5cd8cd1a0ed06416f47348bf
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-01 13:30:14 -0300

    Colocar estilos CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import  { App as HelloWorldApp } from './HelloWorldApp'
-import  { FirstApp } from './FirstApp'
+import  { FirstApp } from './FirstApp';
+import  './style.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
