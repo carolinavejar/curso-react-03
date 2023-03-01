55df5c2befb373d2e133ba456c599392195a5491
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-01 13:26:24 -0300

    Impresion de variables en HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,22 @@
-export const FirstApp = ()=> {
+const newMessage = {
+    message: "Hola mundo",
+    title: "Caro"
+};
+
+// NO PUEDE MANDARSE DIRECMANTE UN OBJETO, HAY QUE PASARLO A STRING
+
+
+const fnTest =  ()=> { 
+    return "Carito desde una Fn"
+}
+
+export const FirstApp = ()=> { 
     return (
     <>
-        <h1>Carito</h1>
+        <h1>Primera aplicación</h1>
+        <h2> { fnTest() } </h2>
+        
+        {/* <code>{ JSON.stringify(newMessage) }</code> */}
         <p>Vejar</p>
     </>);
 };
