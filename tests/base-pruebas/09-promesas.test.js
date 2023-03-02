2dc3c46f308d88844fc5a89a792d1d871d7b18a6
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-02 15:26:10 -0300

    Pruebas con tareas asincronas

---
+++ tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,25 @@
+import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
+
+describe('Pruebas en 08-imp-exp', ()=> {
+
+    test('getHeroeByIdAsync debe retornar un heroe', (done)=> {
+        const id = 1;
+        getHeroeByIdAsync(id).then( hero => {
+
+            expect(hero).toEqual({
+                id: 1,
+                name: 'Batman',
+                owner: 'DC'
+            },)
+            done();
+        })
+    });
+
+    test('getHeroeByIdAsync debe obtener error si el heroe no exite', (done)=> {
+        const id = 1000;
+        getHeroeByIdAsync(id).catch( err => {
+            expect(err).toEqual('No se pudo encontrar el héroe ' + id )
+            done();
+        })
+    });
+})
