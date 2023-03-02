ce5f811579dee8c2d84577702371f44ea72462c4
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-01 23:49:30 -0300

    Jest-Expect-toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,4 +1,12 @@
-test('Esta prueba no puede fallar', ()=> {
-    if (1 === 0) 
-    throw new Error ('0 = 0 ....')
-})
+describe('Pruebas en DemoComponent', ()=> {
+    test('Esta prueba no puede fallar', ()=> {
+    // 1. Inicializacion
+    const message1 = "Hola Mundo";
+
+    // 2. Estímulo
+    const message2 = message1.trim();
+
+    // 3. Observar comportamiento
+    expect(message1).toBe(message2); 
+})})
+
