f96c2b58b527bf7d559a5a450353aaa81ea5fb4a
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-03 00:23:50 -0300

    Probar FirstApp

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -8,7 +8,19 @@ import { FirstApp } from "../src/FirstApp";
 describe('Pruebas en First App', ()=> {
     test('Debe hacer tach con el snapshot', ()=> {
         const title = "Titulo ...";
-        render(< FirstApp title= {title} />)
+        const { container } = render(< FirstApp title= {title} />);
+
+        expect (container).toMatchSnapshot();
+    });
+
+    test('Debe mostrar titulo en h1', ()=> {
+        const title = "Titulo ...";
+        const { container, getByText } = render(< FirstApp title= {title} />);
+
+        expect (getByText(title)).toBeTruthy();
+
+        const h2 = container.querySelector('h2');
+        expect (h2.innerHTML).toContain(title);
     });
 
 
